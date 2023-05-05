0932d2e1cc297516543fa9c0ff894e65e2b4ab22
Míra Jakeš <jakes@grapesc.cz>
2023-05-05 11:14:35 +0200

    feature: command add user is optionally non-interactive

--- commands/UserCommand.php
+++ commands/UserCommand.php
@@ -7,6 +7,7 @@ use Nette\Security\Passwords;
 use Nette\Utils\Validators;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
@@ -21,52 +22,80 @@ class UserCommand extends Command
 
 	public function configure()
 	{
-		$this->setName('user:add');
+		$this->setName('user:add')
+			->addArgument('name', InputArgument::OPTIONAL)
+			->addArgument('email', InputArgument::OPTIONAL)
+			->addArgument('password', InputArgument::OPTIONAL);
+	}
+
+
+	public function nameValidator($val)
+	{
+		if (strlen($val) <= 3) {
+			throw new RuntimeException("Zvolte jmeno alespon 4 znaky dlouhe.");
+		} else if (strlen($val) > 15) {
+			throw new RuntimeException("Jmeno nesmi byt delsi nez 15 znaku.");
+		}
+		if ($this->userModel->getItemBy($val, 'name')) {
+			throw new RuntimeException("Tohle jmeno je jiz pouzito, zvolte jine.");
+		}
+
+		return $val;
+	}
+
+
+	public function emailValidator($val)
+	{
+		if (!Validators::isEmail($val)) {
+			throw new RuntimeException("Zadejte prosim platny email.");
+		}
+		if ($this->userModel->getItemBy($val, 'email')) {
+			throw new RuntimeException("Tento email je jiz pouzit, zvolte jiny.");
+		}
+
+		return $val;
+	}
+
+
+	public function passValidator($val)
+	{
+		if (strlen($val) <= 4) {
+			throw new RuntimeException("Zvolte heslo alespon 5 znaku dlouhe.");
+		}
+
+		return $val;
 	}
 
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$question = $this->getHelper('question');
-		
-		$nameQuestion = new Question('<comment>Jmeno:</comment> ');
-		$nameQuestion->setValidator(function($val) {
-			if (strlen($val) <= 3) {
-				throw new RuntimeException("Zvolte jmeno alespon 4 znaky dlouhe.");
-			} else if (strlen($val) > 15) {
-				throw new RuntimeException("Jmeno nesmi byt delsi nez 15 znaku.");
-			}
-			if ($this->userModel->getItemBy($val, 'name')) {
-				throw new RuntimeException("Tohle jmeno je jiz pouzito, zvolte jine.");
-			}
-			return $val;
-		});
-		
-		$name = $question->ask($input, $output, $nameQuestion);
-		
-		$emailQuestion = new Question('<comment>E-mail:</comment> ');
-		$emailQuestion->setValidator(function($val) {
-			if (!Validators::isEmail($val)) {
-				throw new RuntimeException("Zadejte prosim platny email.");
-			}
-			if ($this->userModel->getItemBy($val, 'email')) {
-				throw new RuntimeException("Tento email je jiz pouzit, zvolte jiny.");
-			}
-			return $val;
-		});
-		
-		$email = $question->ask($input, $output, $emailQuestion);
-		
-		$passwordQuestion = new Question('<comment>Password: [pass]</comment> ', 'pass');
-		$passwordQuestion->setHidden(true);
-		$passwordQuestion->setValidator(function($val) {
-			if (strlen($val) <= 4) {
-				throw new RuntimeException("Zvolte heslo alespon 5 znaku dlouhe.");
-			}
-			return $val;
-		});
-		
-		$password = $question->ask($input, $output, $passwordQuestion);
+
+		if ($input->getArgument('name') && $input->getArgument('email') && $input->getArgument('password')) {
+			$name = $input->getArgument('name');
+			$email = $input->getArgument('email');
+			$password = $input->getArgument('password');
+
+			$this->nameValidator($name);
+			$this->emailValidator($email);
+			$this->passValidator($password);
+		} else {
+			$nameQuestion = new Question('<comment>Jmeno:</comment> ');
+			$nameQuestion->setValidator([$this, 'nameValidator']);
+
+			$name = $question->ask($input, $output, $nameQuestion);
+
+			$emailQuestion = new Question('<comment>E-mail:</comment> ');
+			$emailQuestion->setValidator([$this, 'emailValidator']);
+
+			$email = $question->ask($input, $output, $emailQuestion);
+
+			$passwordQuestion = new Question('<comment>Password: [pass]</comment> ', 'pass');
+			$passwordQuestion->setHidden(true);
+			$passwordQuestion->setValidator([$this, 'passValidator']);
+
+			$password = $question->ask($input, $output, $passwordQuestion);
+		}
 
 		$this->userModel->insert([
 			'name'     => $name,
